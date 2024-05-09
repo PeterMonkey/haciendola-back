7d5a8f345a5f616817c48f0db2b469d087e0c7bb
Pedro Fernandez <pefernandez1408@gmail.com>
2024-05-09 18:32:09 -0400

    ADD: service: create user

--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from '../repository/user.repository';
+import { User } from '../entities/user.entity';
+import { CreateUserDTO } from '../dto/createUser.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectRepository(User) private userRepository: UserRepository) {}
+
+  async create(data: CreateUserDTO) {
+    const { name, email, password } = data;
+    try {
+      const user = await this.userRepository.findOneBy({ email });
+      if (!user) {
+        const hash = bcrypt.hashSync(password, 16);
+        const newUser = this.userRepository.create({
+          name,
+          email,
+          password: hash,
+        });
+        await this.userRepository.save(newUser);
+        return {
+          ok: true,
+          message: 'new user created',
+        };
+      }
+      return {
+        ok: false,
+        message: 'user already exists',
+      };
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
+  }
+}
